a1ecd7f9b4c310c0ad99a2af43e4a8896661fafe
avsoundart <95840730+avsoundart@users.noreply.github.com>
2022-06-04 23:17:25 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -4,31 +4,3 @@
  function playMusic() {
      music.play();
  }
- 
- logo.addEventListener('click', playMusic);
-
- let one = document.querySelector('.one');
- let oneSay = document.querySelector('.curly');
-
- let textHere = document.querySelector('.quote');
-
- let two = document.querySelector('.two');
- let twoSay = document.querySelector('.wavy');
-
- let three = document.querySelector('.three');
- let threeSay = document.querySelector('.tots');
-
-function showCaption() {
-    one.addEventListener('mouseover', function() {
-        textHere.style.visibility = "visible";
-        textHere.textContent = oneSay.innerText;
-    })
-
-    two.addEventListener('mouseover', function() {
-        textHere.textContent = twoSay.innerText;
-    })
-
-    three.addEventListener('mouseover', function() {
-        textHere.textContent = threeSay.innerText;
-    })
-}
