e55a86ae6a709a9aa119846835deb3916127b506
avsoundart <95840730+avsoundart@users.noreply.github.com>
2022-06-11 09:02:05 -0500

    Add files via upload

--- guess-script.js
+++ guess-script.js
@@ -8,46 +8,61 @@ let reset = document.querySelector(".reset"); //Reset
 let randomNumber = Math.trunc(Math.random() * 10) + 1;
 let win = new Audio('chimes.mp3');
 
-    roll.addEventListener("click", function() {
-        let x = Number(guessNumber.value);
+// Result for Guessed Number
+function greenColors() {
+    numberSlot.classList.add("changeGreen");
+    playAgain.classList.add("gold");
+    win.play();
+    numberSlot.textContent = `You Guessed It!`;
+    gamePlays.textContent = `Game Plays ${gameScore++}`;
+}
+// Result for Incorrect Number
+function redColors() {
+    numberSlot.textContent = `My number was ${randomNumber}!`;
+    numberSlot.classList.add("changeRed");
+    gamePlays.textContent = `Game Plays ${gameScore--}`;
+    playAgain.classList.add("gold");
+}
+// Out of Game Plays
+function gameStuff() {
+    gamePlays.textContent = `Out of Game Plays`;
+    numberSlot.textContent = `Play Again.`;
+    reset.classList.add("gold");
+    playAgain.classList.remove("gold");
+}
+// Press Play Again Button
+function playAgainCode() {
+    guessNumber.value = " ";
+    numberSlot.textContent = "?";
+    randomNumber = Math.trunc(Math.random() * 10) + 1;
+    numberSlot.classList.remove("changeRed", "changeGreen");
+    playAgain.classList.remove("gold");
+}
+// Main Game Code
+roll.addEventListener("click", function () {
+    let x = Number(guessNumber.value);
+    if (x === randomNumber) {
+        greenColors();
+    } if (x !== randomNumber) {
+        redColors();
+    } if (x === 0 || x > 10) {
+        numberSlot.textContent = "Enter # from 1 - 10";
+        numberSlot.classList.remove("changeRed");
+    }
 
-        if (x === randomNumber) {
-            win.play();
-           numberSlot.textContent = `You Guessed It!`;
-           numberSlot.classList.add("changeGreen");
-           gamePlays.textContent = `Game Plays ${gameScore++}`;
-           playAgain.classList.add("gold");
-        } if (x !== randomNumber) {
-            numberSlot.textContent = `My number was ${randomNumber}!`;
-            numberSlot.classList.add("changeRed");
-            gamePlays.textContent = `Game Plays ${gameScore--}`;
-            playAgain.classList.add("gold");
-        } if (x === 0 || x > 10) {
-            numberSlot.textContent = "Enter # from 1 - 10";
-            numberSlot.classList.remove("changeRed");
-        }
+    playAgain.addEventListener("click", function () {
+        playAgainCode();
+    });
 
-        playAgain.addEventListener("click", function() {
-            guessNumber.value = " ";
-            numberSlot.textContent = "?";
-            randomNumber = Math.trunc(Math.random() * 10) + 1;
-            numberSlot.classList.remove("changeRed","changeGreen");
-            playAgain.classList.remove("gold");
-        });
+    if (gameScore <= 0) {
+        gameStuff();
+    };
 
-        if (gameScore <= 0) {
-            gamePlays.textContent = `Out of Game Plays`;
-            numberSlot.textContent = `Play Again.`;
-            reset.classList.add("gold");
-            playAgain.classList.remove("gold");
-        };
-
-        reset.addEventListener("click", function() {
-            window.location.reload();
-            reset.classList.remove("gold");
-            
-        });
+    reset.addEventListener("click", function () {
+        window.location.reload();
+        reset.classList.remove("gold");
 
     });
 
-    
+});
+
